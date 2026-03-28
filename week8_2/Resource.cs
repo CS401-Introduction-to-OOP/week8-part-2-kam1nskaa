8c1baac6b69044fbda5e0787b9891776eaacac9c
kam1nskaa <marta.kaminska.25@kse.org.ua>
2026-03-28 15:31:01 +0200

    Class Resource added

---
+++ week8_2/Resource.cs
@@ -0,0 +1,11 @@
+﻿namespace week8_2;
+
+public abstract class Resource
+{
+    public string Name { get; }
+    public bool IsOpen { get; protected set; }
+
+    public abstract void Open();
+    public abstract void Close();
+    
+}
