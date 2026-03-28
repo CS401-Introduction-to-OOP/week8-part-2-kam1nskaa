01d9ced27ea7f77f9dea9ca83ccbac4f8c39570a
kam1nskaa <marta.kaminska.25@kse.org.ua>
2026-03-28 15:52:12 +0200

    Class NetworkResource added

---
+++ week8_2/NetworkResource.cs
@@ -0,0 +1,23 @@
+﻿namespace week8_2;
+
+public class NetworkResource : Resource, IDisposable
+{
+    public NetworkResource(string name) : base(name)
+    {
+    }
+    public override void Open()
+    {
+        IsOpen = true;
+        throw new NotImplementedException();
+    }
+    public override void Close()
+    {
+        IsOpen = false;
+        throw new NotImplementedException();
+    }
+    public void Dispose()
+    {
+        Console.WriteLine($"[Diagnostic], Dispose for{Name}");
+        Close();
+    }
+}
