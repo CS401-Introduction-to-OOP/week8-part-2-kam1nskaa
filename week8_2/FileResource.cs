f2c4f05eff3db86e76a81d5450bc14002cd46a3f
kam1nskaa <marta.kaminska.25@kse.org.ua>
2026-03-28 15:41:25 +0200

    Class FileResource added

---
+++ week8_2/FileResource.cs
@@ -0,0 +1,20 @@
+﻿namespace week8_2;
+
+public class FileResource: Resource, IDisposable
+{
+    public FileResource(string name) : base(name) { }
+
+    public override void Open()
+    {
+        IsOpen = true;
+    }
+    public override void Close()
+    {
+        IsOpen = false;
+
+    }
+    public void Dispose()
+    {
+        Close();
+    }
+}
