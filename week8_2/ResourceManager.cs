c4e2d0a8796001ea5afa9054bafad0fdf92f67af
kam1nskaa <marta.kaminska.25@kse.org.ua>
2026-03-28 15:57:00 +0200

    Class ResourseManager added

---
+++ week8_2/ResourceManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace week8_2;
+
+public class ResourceManager<T> where T : Resource
+{
+    private readonly List<T> _resources = new();
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+        throw new NotImplementedException();
+    }
+   
+}
