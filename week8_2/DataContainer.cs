40dc62dfaebb66496357329a0438b1113cd038a5
kam1nskaa <marta.kaminska.25@kse.org.ua>
2026-03-28 16:48:01 +0200

    Task 3 added

---
+++ week8_2/DataContainer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+namespace week8_2;
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+    public long GetTotalSize()
+    {
+        return _items.Count();
+    }
+}
--- week8_2/Program.cs
+++ week8_2/Program.cs
@@ -1,2 +1,17 @@
-﻿namespace week8_2;
+﻿using week8_2;
 
+var container = new DataContainer<DataItem>();
+for (int i = 0; i < 10000; i++)
+{
+    var item = new DataItem
+    {
+        Id = $"item-{i}",
+        Payload = new byte[1024]
+    };
+    container.AddItem(item);
+}
+Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+// Size did not change because Conteiner saves links 
+GC.Collect();
+Console.WriteLine("GC.Collect() was called.");
+Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
